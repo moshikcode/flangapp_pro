5a0b10acfb15fc811aa5ab5f02305a6b23085efe
moshikcode <108571486+moshikcode@users.noreply.github.com>
2025-07-17 17:21:05 +0300

    update for file android/app/build.gradle.kts - 17-07-2025 14:21

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "pro.flangapp.app"
+        applicationId = "il.co.mako"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
@@ -34,9 +34,9 @@ android {
     signingConfigs {
         create("release") {
             storeFile = file("sign/key.jks")
-            storePassword = "EMrNEJbNkbPs"
-            keyAlias = "tadapp_android_e"
-            keyPassword = "EMrNEJbNkbPs"
+            storePassword = "bcoOalmUbaPKLhXR"
+            keyAlias = "RJtcqNSOSp"
+            keyPassword = "bcoOalmUbaPKLhXR"
         }
     }
 
